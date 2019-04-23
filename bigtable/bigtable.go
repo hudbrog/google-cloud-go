dd8805f114d009a0b4b55105a5194047881116ef
Chen Liang <code@chen.technology>
2019-04-22 23:26:21 -0700

    bigtable: btpb.RowSet as RowSet

--- bigtable/bigtable.go
+++ bigtable/bigtable.go
@@ -17,6 +17,7 @@ limitations under the License.
 package bigtable // import "cloud.google.com/go/bigtable"
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
@@ -263,6 +264,104 @@ type RowSet interface {
 
 	// Valid reports whether this set can cover at least one row.
 	valid() bool
+}
+
+type RowsRanges btpb.RowSet
+
+func (r RowsRanges) proto() *btpb.RowSet {
+	rs := btpb.RowSet(r)
+	return &rs
+}
+
+func (r RowsRanges) retainRowsAfter(lastRowKey string) RowSet {
+	res := RowsRanges{}
+	for i := range r.RowKeys {
+		if string(r.RowKeys[i]) > lastRowKey {
+			res.RowKeys = append(res.RowKeys, r.RowKeys[i])
+		}
+	}
+	for i := range r.RowRanges {
+		var (
+			newRange = &btpb.RowRange{
+				StartKey: r.RowRanges[i].StartKey,
+				EndKey:   r.RowRanges[i].EndKey,
+			}
+			invalid bool
+		)
+
+		switch rr := r.RowRanges[i].EndKey.(type) {
+		case *btpb.RowRange_EndKeyOpen:
+			if string(rr.EndKeyOpen) <= lastRowKey {
+				continue
+			}
+		case *btpb.RowRange_EndKeyClosed:
+			if string(rr.EndKeyClosed) <= lastRowKey {
+				continue
+			}
+		}
+
+		switch rr := r.RowRanges[i].StartKey.(type) {
+		case *btpb.RowRange_StartKeyOpen:
+			if string(rr.StartKeyOpen) <= lastRowKey {
+				newRange.StartKey = &btpb.RowRange_StartKeyOpen{
+					StartKeyOpen: []byte(lastRowKey),
+				}
+			}
+		case *btpb.RowRange_StartKeyClosed:
+			if string(rr.StartKeyClosed) <= lastRowKey {
+				newRange.StartKey = &btpb.RowRange_StartKeyOpen{
+					StartKeyOpen: []byte(lastRowKey),
+				}
+			}
+		}
+		if !invalid {
+			res.RowRanges = append(res.RowRanges, newRange)
+		}
+	}
+	return res
+}
+
+func (r RowsRanges) valid() bool {
+	if len(r.RowKeys) > 0 {
+		return true
+	}
+	if len(r.RowKeys) == 0 && len(r.RowRanges) == 0 {
+		return false
+	}
+
+	for i := range r.RowRanges {
+		if r.RowRanges[i].StartKey == nil || r.RowRanges[i].EndKey == nil {
+			return true
+		}
+		if btRowRangeValid(r.RowRanges[i]) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func btRowRangeValid(rr *btpb.RowRange) bool {
+	if rr.StartKey == nil || rr.EndKey == nil {
+		return true
+	}
+	switch rs := rr.StartKey.(type) {
+	case *btpb.RowRange_StartKeyOpen:
+		switch re := rr.EndKey.(type) {
+		case *btpb.RowRange_EndKeyOpen:
+			return bytes.Compare(rs.StartKeyOpen, re.EndKeyOpen) < 0
+		case *btpb.RowRange_EndKeyClosed:
+			return bytes.Compare(rs.StartKeyOpen, re.EndKeyClosed) < 0
+		}
+	case *btpb.RowRange_StartKeyClosed:
+		switch re := rr.EndKey.(type) {
+		case *btpb.RowRange_EndKeyOpen:
+			return bytes.Compare(rs.StartKeyClosed, re.EndKeyOpen) < 0
+		case *btpb.RowRange_EndKeyClosed:
+			return bytes.Compare(rs.StartKeyClosed, re.EndKeyClosed) <= 0
+		}
+	}
+	return false
 }
 
 // RowList is a sequence of row keys.
