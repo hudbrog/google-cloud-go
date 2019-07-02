6380e9b618acb674385dd3cb0e08fa041b723321
Chen Liang <code@chen.technology>
2019-07-02 12:23:46 -0700

    bigtable/filter: fixed PassallFilter

--- bigtable/filter.go
+++ bigtable/filter.go
@@ -348,7 +348,7 @@ type passAllFilter struct{}
 
 func (paf passAllFilter) String() string { return "passAllFilter()" }
 
-func (paf passAllFilter) proto() *btpb.RowFilter {
+func (paf passAllFilter) Proto() *btpb.RowFilter {
 	return &btpb.RowFilter{Filter: &btpb.RowFilter_PassAllFilter{PassAllFilter: true}}
 }
 
